9fb36202cb71d2cdb9c91b4fa9f5dc336a4e463e
CoalaJoe <ashura@protonmail.ch>
2017-09-08 15:01:41 +0200

    Fixes #1: Did the thing with the variables and such.

--- Aufgabe 3/sizeof/main.cpp
+++ Aufgabe 3/sizeof/main.cpp
@@ -2,13 +2,18 @@
 
 int main()
 {
+    double var_double = 32423.234;
+    long double var_long_double = 2423423423423.234234;
+    char var_char = 'x';
+    std::string var_string = "Here we goo..";
+
     std::cout << sizeof(int) << "\n";
     std::cout << sizeof(long int) << "\n";
     std::cout << sizeof(float) << "\n";
-    std::cout << sizeof(double) << "\n";
-    std::cout << sizeof(long double) << "\n";
-    std::cout << sizeof(char) << "\n";
-    std::cout << sizeof(std::string) << "\n";
+    std::cout << sizeof(var_double) << "\n";
+    std::cout << sizeof(var_long_double) << "\n";
+    std::cout << sizeof(var_char) << "\n";
+    std::cout << sizeof(var_string) << "\n";
 
     return 0;
 }
