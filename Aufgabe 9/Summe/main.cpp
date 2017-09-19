c67aad59aa891f0109d25f4eb08563faf641688d
CoalaJoe <ashura@protonmail.ch>
2017-09-19 14:52:40 +0200

    Finished lesson 9

--- Aufgabe 9/Summe/main.cpp
+++ Aufgabe 9/Summe/main.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 
+/**
+ * Returns a recursively added result.
+ *
+ * @param num
+ * @return
+ */
+int sum(int num);
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << sum(10);
     return 0;
 }
 
 int sum(int num)
 {
+    int result{0};
 
+    for (int i = 0; i <= num; ++i) {
+        result += i;
+    }
+
+    return result;
 }
