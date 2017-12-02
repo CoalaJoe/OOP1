7ea664f403750a050cb4a40842ed0e8cb0a774d8
CoalaJoe <ashura@protonmail.ch>
2017-12-02 10:03:11 +0100

    Fixed shit

--- Aufgabe 12/Zeichen zählen/main.cpp
+++ Aufgabe 12/Zeichen zählen/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 
+int countShit(const char string[250]){
+    for (int i = 0; i < 250; ++i) {
+        if (string[i] == '\0') {
+            return i;
+        }
+    }
+    return 250;
+}
+
 int main()
 {
+    std::string tmp;
+    char test[250];
+    
+    std::cout << "Geben Sie einen String ein: ";
+    std::cin.get(test, 250);
 
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Der String enthält " << countShit(test) << " Zeichen!\n";
+
     return 0;
 }
