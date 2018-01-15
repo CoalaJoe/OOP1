46ba8ca338efd4ac1c0ecd7f55f151746129d793
Ashura <ashura@protonmail.ch>
2018-01-15 17:32:28 +0100

    Closes #3: Inplement logic to only apply cipher to chars and loop from  back to

--- Aufgabe 12/Caesar Chiffre/main.cpp
+++ Aufgabe 12/Caesar Chiffre/main.cpp
@@ -4,6 +4,21 @@ char *encrypt(char c[250], int key);
 
 char *decrypt(char c[250], int key);
 
+
+const int UPPER_START = 65;
+const int UPPER_STOP  = 90;
+
+const int LOWER_START = 97;
+const int LOWER_STOP  = 122;
+
+const int CHAR_RANGE = 26;
+
+/**
+ * A = 65, Z = 90
+ * a = 97, z = 122
+ *
+ * @return
+ */
 int main()
 {
     int(key);
@@ -23,13 +38,44 @@ int main()
     return 0;
 }
 
+char getNextChar(char c, int key)
+{
+    bool isChar = false;
+    auto ascii  = (int) c;
+
+    if (ascii <= LOWER_STOP && ascii >= LOWER_START) {
+        isChar = true;
+        std::cout << ascii << "\n";
+        if ((ascii + key) > LOWER_STOP) {
+            ascii = ascii - CHAR_RANGE;
+        } else if ((ascii + key) < LOWER_START) {
+            ascii = ascii + CHAR_RANGE;
+        }
+    }
+
+    if (ascii <= UPPER_STOP && ascii >= UPPER_START) {
+        isChar = true;
+        if ((ascii + key) > UPPER_STOP) {
+            ascii = ascii - CHAR_RANGE;
+        } else if ((ascii + key) < UPPER_START) {
+            ascii = ascii + CHAR_RANGE;
+        }
+    }
+
+    if (isChar) {
+        ascii += key;
+    }
+
+    return (char) ascii;
+}
+
 char *encrypt(char c[250], int key)
 {
     for (int i = 0; i < 250; ++i) {
         if (c[i] == '\0') {
             break;
         }
-        c[i] = char(int(c[i]) + key);
+        c[i] = getNextChar(c[i], key);
     }
 
     return c;
@@ -41,7 +87,7 @@ char *decrypt(char c[250], int key)
         if (c[i] == '\0') {
             break;
         }
-        c[i] = char(int(c[i]) - key);
+        c[i] = getNextChar(c[i], -key);
     }
 
     return c;
