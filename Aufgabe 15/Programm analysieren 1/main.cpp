809874ef9f94e9da35783ee3460e1302359c1a68
Ashura <ashura@protonmail.ch>
2018-02-25 14:34:06 +0100

    Add code analysis

--- Aufgabe 15/Programm analysieren 1/main.cpp
+++ Aufgabe 15/Programm analysieren 1/main.cpp
@@ -15,3 +15,17 @@ int main()
     cout << "b = ? " << b << endl;
     return 0;
 }
+
+
+/*
+    int a(6), b(2);
+    int *pa, *pb;
+    pa = &a;                            || pa = 6
+    *pa += 1;                           || dereference pa and add +1 to 6
+    cout << "a = ? " << a << endl;      || Should print 7.
+    pb = &b;                            || pb = 2
+    *pb = *pb * *pa;                    || dereference pb and set it equal to pb * dereferenced pa. pb = 2 * 7 = 14
+    b = b++ + ++a;                      || b = 14 + 7 + 1 => 22 | Add 1 to a. => 8
+    cout << "b = ? " << b << endl;      || Should print 22.
+    return 0;
+*/
--- Aufgabe 15/Programm analysieren 2/main.cpp
+++ Aufgabe 15/Programm analysieren 2/main.cpp
@@ -22,3 +22,23 @@ int main()
     cout << "++*f4: " << ++*f4 << endl;
     return 0;
 }
+
+/*
+    float *f1, f2, f3(1), *f4;          || f3 = 1
+    f2 = 23;                            || f2 = 23
+    float &f5 = f2;                     || f5 = &f2
+    f1 = &f5;                           || f1 = &f5(f2) = 23
+    *f1 = 5;                            || f2 = 5
+    f1 = NULL;                          || f1 = null
+    cout << "f1: " << f1 << endl;       || Should print null
+    cout << "f2: " << f2 << endl;       || Should print 5
+    f4 = f1 = &f2;                      || f4 = f1 = &f2
+    *f1 = 23;                           || f4, f1, f2 = 23
+    f4 = &f3;                           || f4 = &f3
+    f3 = 17;                            || f4, f3 = 17
+    cout << "f1: " << f1 << endl;       || Should print a memory address
+    cout << "f2: " << f2 << endl;       || Should print 23
+    cout << "f3: " << f3 << endl;       || Should print 17
+    cout << "++*f4: " << ++*f4 << endl; || Should print 18
+    return 0;
+*/
